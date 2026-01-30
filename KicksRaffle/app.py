de6ab2dc1c1bc3e001971a24823244bb3967d4bf
guillermofraga <guillermofragaquiroga@gmail.com>
2026-01-30 18:35:31 +0100

    add kicksRaffles Migracion Inicial

--- KicksRaffle/app.py
+++ KicksRaffle/app.py
@@ -1,12 +1,6 @@
 from app import create_app
-from config import Config
-from flask_migrate import Migrate
-from models import db
 
 app = create_app()
-app.config.from_object(Config)
-migrate = Migrate(app, db)
-
 
 if __name__ == '__main__':
     app.run(debug=app.config["DEBUG"])
