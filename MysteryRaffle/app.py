89023b239e1df68ee754a8afe70c2986f11bbe66
guillermofraga <guillermofragaquiroga@gmail.com>
2026-01-31 19:45:30 +0100

    Changed configuration in __init__.py by app.py in MysteryRaffle

--- MysteryRaffle/app.py
+++ MysteryRaffle/app.py
@@ -1,6 +1,24 @@
-from routes import create_app
+from flask import Flask
+from config import Config
+from flask_migrate import Migrate
+from models import db
+from routes.main import main_bp
+from routes.raffle import raffle_bp
 
-app = create_app()
+app = Flask(__name__)
+app.config.from_object(Config)
+
+#Inicializar la base de datos
+try:
+    db.init_app(app)
+    migrate = Migrate(app, db)
+except Exception as e:
+    print(f"Error initializing database: {e}")
+
+#Registrar blueprints
+app.register_blueprint(main_bp)
+app.register_blueprint(raffle_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=app.config["DEBUG"])
