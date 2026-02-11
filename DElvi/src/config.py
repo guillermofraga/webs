a5e6029cec98879d7661c8fd0d098f751155a7f1
guillermofraga <guillermofragaquiroga@gmail.com>
2026-02-11 15:52:15 +0100

    I dont know what changed I made in config.py on Delvi

--- DElvi/src/config.py
+++ DElvi/src/config.py
@@ -15,7 +15,7 @@ class Config:
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
     # Correo del administrador para recibir copias de confirmaciones
-    ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")  
+    ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
 
     # Configuración de depuración
-    DEBUG = os.getenv("FLASK_DEBUG", "False").lower() in ["true", "1"] # Convertir a booleano, Obtiene el valor la variable de entorno, la convierte en minúsculas y comprueba si el valor esta en la lista ["true", "1"], si esta en la lista devuelve True, si no devuelve False. 
+    DEBUG = os.getenv("FLASK_DEBUG", "False").lower() in ["true", "1"] # Convertir a booleano, Obtiene el valor la variable de entorno, la convierte en minúsculas y comprueba si el valor esta en la lista ["true", "1"], si esta en la lista devuelve True, si no devuelve False.
