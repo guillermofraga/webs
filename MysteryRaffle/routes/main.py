512a345c63f7fb570b8ad0b011033ea291c31c7a
guillermofraga <guillermofragaquiroga@gmail.com>
2026-04-02 15:40:34 +0200

    add robots.txt and sitemap.xml on mysteryraffle

--- MysteryRaffle/routes/main.py
+++ MysteryRaffle/routes/main.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
+
+STATICFOLDER = '../static'  # Define la ruta a la carpeta de archivos estáticos
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
+
+# Rutas para archivos estáticos especiales
+@main_bp.route("/robots.txt")
+def robots():
+    return send_from_directory(STATICFOLDER, "robots.txt")
+
+@main_bp.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory(STATICFOLDER, "sitemap.xml")
